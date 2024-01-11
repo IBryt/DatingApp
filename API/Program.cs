ab7335a890f8c262b66887b461cfd29db04a7ea7
ihorbryt@gmail.com <36583283+IBryt@users.noreply.github.com>
2024-01-11 10:41:01 +0200

    Initial commit

---
+++ API/Program.cs
@@ -0,0 +1,18 @@
+using API;
+
+namespace IgorBryt.Store.WebAPI;
+
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
