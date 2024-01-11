9c371818084f416ebcac27d6d174597bb2154bef
ihorbryt@gmail.com <36583283+IBryt@users.noreply.github.com>
2024-01-11 10:45:33 +0200

    Building a walking skeleton of API part 2

--- API/Startup.cs
+++ API/Startup.cs
@@ -14,15 +14,12 @@ public class Startup
         services.AddControllers();
 
         services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
         if (env.IsDevelopment())
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
             app.UseDeveloperExceptionPage();
         }
 
