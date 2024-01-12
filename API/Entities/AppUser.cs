f5cd795d5785af168ac0900bf2928ea21c516710
ihorbryt@gmail.com <36583283+IBryt@users.noreply.github.com>
2024-01-12 09:52:19 +0200

    Authentication Basics Part 6

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,5 +3,7 @@
 public class AppUser
 {
     public int Id { get; set; }
-    public string? UserName { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public byte[] PasswordHash { get; set; } = Array.Empty<byte>();
+    public byte[] PasswordSalt { get; set; } = Array.Empty<byte>();
 }
