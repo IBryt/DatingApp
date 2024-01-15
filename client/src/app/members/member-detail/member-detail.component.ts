efba755fed28bfead929ff3b29c95368028f97bf
ihorbryt@gmail.com <36583283+IBryt@users.noreply.github.com>
2024-01-15 12:34:35 +0200

    Routing to Detail Page in Asp.Net Core part 55

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/_model/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
   styleUrls: ['./member-detail.component.css']
 })
-export class MemberDetailComponent {
+export class MemberDetailComponent implements OnInit {
+  member: Member = <Member>{};
 
+  constructor(
+    private membersService: MembersService,
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.loadMembers();  
+  }
+
+  loadMembers() {
+    this.membersService.getMember(this.route.snapshot.paramMap.get('username')!).subscribe({
+      next: member => this.member = member,
+    })
+  }
 }
