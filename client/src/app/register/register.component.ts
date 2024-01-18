d1215da4062fd085daf50be0e0f0088dc92f10ed
ihorbryt@gmail.com <36583283+IBryt@users.noreply.github.com>
2024-01-18 10:37:29 +0200

    Reactive forms introduction in Asp.Net Core part 73

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { FormControl, FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -11,21 +12,35 @@ import { ToastrService } from 'ngx-toastr';
 export class RegisterComponent {
   @Output() cancelRegister: any = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(
     private accountService: AccountService,
     private toastr: ToastrService,
-  ) { }
+  ) {
+    this.registerForm = this.initializeForm();
+  }
+
+  initializeForm(): FormGroup {
+    const fg = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl(),
+    });
+
+    return fg;
+  }
 
   register() {
-    this.accountService.register(this.model).subscribe({
-      next: user => {
-        console.log(user);
-        this.cancel();
-        return user;
-      },
-      error: error => this.toastr.error(error.error)
-    });
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.model).subscribe({
+    //   next: user => {
+    //     console.log(user);
+    //     this.cancel();
+    //     return user;
+    //   },
+    //   error: error => this.toastr.error(error.error)
+    // });
   }
 
   cancel() {
