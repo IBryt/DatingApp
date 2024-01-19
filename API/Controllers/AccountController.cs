38040af37209d101adaab93ea03d37adde128923
ihorbryt@gmail.com <36583283+IBryt@users.noreply.github.com>
2024-01-19 10:22:09 +0200

    Updating the API register method in Asp.Net Core part 80

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using API.DTOs;
 using API.Entities;
 using API.interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
@@ -15,11 +16,13 @@ public class AccountController : ControllerBase
 {
     private readonly DataContext _context;
     private readonly ITokenService _tokenService;
+    private readonly IMapper _mapper;
 
-    public AccountController(DataContext context, ITokenService tokenService)
+    public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
     {
         _context = context;
         _tokenService = tokenService;
+        _mapper = mapper;
     }
 
     [HttpPost("Register")]
@@ -30,14 +33,13 @@ public class AccountController : ControllerBase
             return BadRequest("Username is taken");
         }
 
+        var user = _mapper.Map<AppUser>(registerDto);
+
         using var hmac = new HMACSHA512();
 
-        var user = new AppUser
-        {
-            UserName = registerDto.Username,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-            PasswordSalt = hmac.Key
-        };
+        user.UserName = registerDto.Username;
+        user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+        user.PasswordSalt = hmac.Key;
 
         _context.Users.Add(user);
 
@@ -45,12 +47,7 @@ public class AccountController : ControllerBase
 
         var token = _tokenService.CreateToken(user);
 
-        var userDto = new UserDto
-        {
-            Username = user.UserName,
-            Token = token,
-            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url ?? ""
-        };
+        var userDto = GetUserDto(user, token);
 
         return userDto;
     }
@@ -62,7 +59,7 @@ public class AccountController : ControllerBase
             .Include(x => x.Photos)
             .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
-        if (user == null) 
+        if (user == null)
         {
             return Unauthorized("Invalid username");
         }
@@ -81,14 +78,20 @@ public class AccountController : ControllerBase
 
         var token = _tokenService.CreateToken(user);
 
-        var userDto = new UserDto
+        var userDto = GetUserDto(user, token);
+
+        return userDto;
+    }
+
+    private static UserDto GetUserDto(AppUser user, string token)
+    {
+        return new UserDto
         {
             Username = user.UserName,
             Token = token,
             PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url ?? "",
+            KnownAs = user.KnownAs,
         };
-
-        return userDto;
     }
 
     private async Task<bool> UserExists(string username)
