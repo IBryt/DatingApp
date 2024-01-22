4ecabcd2fc4e10ae90bac4e4d6a4ac58d0e09e52
ihorbryt@gmail.com <36583283+IBryt@users.noreply.github.com>
2024-01-22 14:58:34 +0200

    Setting up the automapper profiles in Asp.Net Core part 109

---
+++ API/DTOs/CreateMessageDto.cs
@@ -0,0 +1,7 @@
+﻿namespace API.DTOs;
+
+public class CreateMessageDto
+{
+    public string RecipientUsername { get; set; } = string.Empty;
+    public string Context { get; set; } = string.Empty;
+}
