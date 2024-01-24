3dbe27285ba616b27d371b909f2422b327effa81
ihorbryt@gmail.com <36583283+IBryt@users.noreply.github.com>
2024-01-24 14:10:20 +0200

    Refactoring and adding a new migration in Asp.Net Core part 127

--- API/Program.cs
+++ API/Program.cs
@@ -16,7 +16,7 @@ public class Program
         {
             var context = services.GetRequiredService<DataContext>();
             await context.Database.MigrateAsync();
-            await Seed.SeedUsers(context);
+            //await Seed.SeedUsers(context);
         }
         catch (Exception ex) 
         {
