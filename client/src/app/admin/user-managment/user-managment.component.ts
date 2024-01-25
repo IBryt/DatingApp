5f7885f52672a108a5e77e40376fff921489e584
ihorbryt@gmail.com <36583283+IBryt@users.noreply.github.com>
2024-01-25 15:23:28 +0200

    Setting up modals in Asp.Net Core part 136

--- client/src/app/admin/user-managment/user-managment.component.ts
+++ client/src/app/admin/user-managment/user-managment.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import { User } from 'src/app/_model/user';
 import { AdminService } from 'src/app/_services/admin.service';
+import { RolesModalComponent } from 'src/app/modals/roles-modal/roles-modal.component';
 
 @Component({
   selector: 'app-user-managment',
@@ -8,10 +10,12 @@ import { AdminService } from 'src/app/_services/admin.service';
   styleUrls: ['./user-managment.component.css']
 })
 export class UserManagmentComponent implements OnInit {
-  users: Partial<User[]> = []
+  users: Partial<User[]> = [];
+  bsModalRef?: BsModalRef;
 
   constructor(
     private adminService: AdminService,
+    private modalService: BsModalService,
   ) { }
 
   ngOnInit(): void {
@@ -22,5 +26,17 @@ export class UserManagmentComponent implements OnInit {
     this.adminService.getUsersWithRoles().subscribe({
       next: resp => this.users = resp
     })
+  }
+
+  openRolesModal() {
+    const initialState: ModalOptions = {
+      initialState: {
+        list: ['Open a modal with component', 'Pass your data', 'Do something else', '...'],
+        title: 'Modal with component'
+      }
+    };
+    this.bsModalRef = this.modalService.show(RolesModalComponent, initialState);
+    this.bsModalRef.content.closeBtnName = 'Close';
+    // this.bsModalRef = this.modalService.show(RolesModalComponent, )
   }
 }
