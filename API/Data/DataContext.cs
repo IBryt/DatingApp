835a9f097800650b3198f098c201e9365e37243f
ihorbryt@gmail.com <36583283+IBryt@users.noreply.github.com>
2024-01-27 23:00:10 +0200

    Fixing UTC dates again in Asp.Net Core part 156

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -2,6 +2,10 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.Reflection;
 
 namespace API.Data;
 
@@ -58,5 +62,81 @@ public class DataContext : IdentityDbContext<AppUser, AppRole, int,
             .HasOne(u => u.Sender)
             .WithMany(u => u.MessagesSent)
             .OnDelete(DeleteBehavior.Restrict);
+
+        builder.ApplyUtcDateTimeConverter();
     }
+}
+
+public class UtcValueConverter : ValueConverter<DateTime, DateTime>
+{
+    public UtcValueConverter()
+        : base(v => v, v => DateTime.SpecifyKind(v, DateTimeKind.Utc))
+    {
+    }
+}
+
+public static class UtcDateAnnotation
+{
+    private const string IsUtcAnnotation = "IsUtc";
+
+    public static PropertyBuilder<TProperty> IsUtc<TProperty>(this PropertyBuilder<TProperty> builder, bool isUtc = true)
+    {
+        if (builder is null)
+        {
+            throw new ArgumentNullException(nameof(builder));
+        }
+
+        return builder.HasAnnotation(IsUtcAnnotation, isUtc);
+    }
+
+    public static bool IsUtc(this IMutableProperty property)
+    {
+        if (property is null)
+        {
+            throw new ArgumentNullException(nameof(property));
+        }
+
+        var attribute = property.PropertyInfo?.GetCustomAttribute<IsUtcAttribute>();
+        if (attribute is not null && attribute.IsUtc)
+        {
+            return true;
+        }
+
+        return ((bool?)property.FindAnnotation(IsUtcAnnotation)?.Value) ?? true;
+    }
+
+    /// <summary>
+    /// Make sure this is called after configuring all your entities.
+    /// </summary>
+    public static void ApplyUtcDateTimeConverter(this ModelBuilder builder)
+    {
+        if (builder is null)
+        {
+            throw new ArgumentNullException(nameof(builder));
+        }
+
+        foreach (var entityType in builder.Model.GetEntityTypes())
+        {
+            foreach (var property in entityType.GetProperties())
+            {
+                if (!property.IsUtc())
+                {
+                    continue;
+                }
+
+                if (property.ClrType == typeof(DateTime) ||
+                    property.ClrType == typeof(DateTime?))
+                {
+                    property.SetValueConverter(typeof(UtcValueConverter));
+                }
+            }
+        }
+    }
+}
+
+[AttributeUsage(AttributeTargets.Property)]
+public class IsUtcAttribute : Attribute
+{
+    public IsUtcAttribute(bool isUtc = true) => this.IsUtc = isUtc;
+    public bool IsUtc { get; }
 }
--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -27,7 +27,5 @@ public class AutoMapperProfiles : Profile
             .ForMember(dest => dest.RecipientPhotoUrl, opt =>
                 opt.MapFrom(src => src.Recipient.Photos.FirstOrDefault(p => p.IsMain).Url));
 
-        CreateMap<DateTime, DateTime>()
-            .ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
     }
 }
